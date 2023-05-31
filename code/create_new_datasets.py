0cb098f30735e4aa35c13e26134c6162f0856309
lior motola <lior.motola@campus.technion.ac.il>
2023-05-31 20:34:28 +0300

    added documentation to create_new_datasets

--- code/create_new_datasets.py
+++ code/create_new_datasets.py
@@ -13,10 +13,22 @@ def create_datasets(original_dataset_path,
                     augmentation_transforms,
                     flip_transforms,
                     together_transforms,
-                    together_class,
+                    together_classes,
                     flip_v):
-    # Set the paths for the original dataset and the destination directories for the new datasets
-
+    """
+    split original data into 3 folds, create 3 new datasets where each has 2 train folds and one val fold
+     and perform augmentations on train data only for each dataset.
+    the new datasets will be saved in dataset_names
+    @param: original_dataset_path - path to the original data directory
+    @param: dataset_names - list of 3 names, one for each dataset created
+    @param: augmentation_transforms: list of augmentations to perform over train data, will be performed over all labels,
+     each augmentation separatly.
+    @param: flip_transforms: list of flip transformations to perform over train data. will be performed only over the labels ["i","ii","iii","x"]
+    @param: together_transforms: list of augmentations to perform over train data.
+            Augmentations will be performed one over each other (one augmented image wil be created). will be performed over labels in @together_classes only
+    @param: together_classes: list of labels on which together_transforms will be performed.
+    @flip_v : boolean argument indicating whether to perform horizontal flip over the labels [v,iv,vi] or not.
+    """
 
     new_dataset_paths = [os.path.join("..", "data",name) for name in dataset_names]
 
@@ -36,11 +48,8 @@ def create_datasets(original_dataset_path,
     kf = KFold(n_splits=k, shuffle=True, random_state=42)
     indices = np.arange(len(original_images))
 
-
     # Iterate over the k-fold indices and create new datasets
     for i, (train_indices, val_indices) in enumerate(kf.split(indices)):
-        train_images = [original_images[idx] for idx in train_indices]
-        val_images = [original_images[idx] for idx in val_indices]
 
         # Create the new dataset directories for the current fold
         new_dataset_path = new_dataset_paths[i]
@@ -51,7 +60,6 @@ def create_datasets(original_dataset_path,
         val_dir = os.path.join(new_dataset_path, "val")
         os.makedirs(train_dir, exist_ok=True)
         os.makedirs(val_dir, exist_ok=True)
-
 
         # Copy and augment the corresponding image files to the train directory
         for idx in train_indices:
@@ -64,15 +72,13 @@ def create_datasets(original_dataset_path,
             for n,transformation in enumerate(augmentation_transforms):
                 # Apply augmentation transforms to the image
                 image = Image.open(image_path)
-
                 augmented_image = transformation(image)
-                    # Save the augmented image
+                # Save the augmented image
                 augmented_image.save(new_image_path.replace(".png",f"_{n}.png"))
 
             if class_name in flip_class:
                 for n, transformation in enumerate(flip_transforms):
                     image = Image.open(image_path)
-
                     augmented_image = transformation(image)
                     # Save the augmented image
                     augmented_image.save(new_image_path.replace(".png", f"_{n+len(augmentation_transforms)}.png"))
@@ -81,7 +87,6 @@ def create_datasets(original_dataset_path,
                 if class_name == "v":
                     image = Image.open(image_path)
                     augmented_image = transforms.RandomHorizontalFlip(1)(image)
-                    # Save the augmented image
                     augmented_image.save(new_image_path.replace(".png", f"_new.png"))
                 elif class_name == "iv":
                     image = Image.open(image_path)
@@ -96,54 +101,52 @@ def create_datasets(original_dataset_path,
                     os.makedirs(os.path.dirname(path), exist_ok=True)
                     augmented_image.save(path.replace(".png", f"_new.png"))
 
-
-            if together_class and class_name in together_class:
+            if together_classes and class_name in together_classes:
                 image = Image.open(image_path)
+                #perform augmentations over each other
                 augmented_image = together_transforms[0](image)
                 for transformation in together_transforms[1:]:
                     augmented_image = transformation(augmented_image)
                 augmented_image.save(new_image_path.replace(".png", f"_{1+len(augmentation_transforms)+len(flip_transforms)}.png"))
 
 
-
-        # Copy and augment the corresponding image files to the validation directory
+        # Copy validation image files to the validation directory without augmentations!
         for idx in val_indices:
             image_path = original_images[idx]
             class_name = os.path.basename(os.path.dirname(image_path))
             new_image_path = os.path.join(val_dir, class_name, os.path.basename(image_path))
             os.makedirs(os.path.dirname(new_image_path), exist_ok=True)
-
             shutil.copy(image_path, new_image_path)
 
 
 
 def exposure(image):
-    # Convert the image to a PyTorch tensor
-    image_tensor = TF.to_tensor(image)
+    """
+    perform exposure augmentation over an image. intensity = 2.5
+    """
 
-    # Adjust the exposure
-    exposure_factor = 2.5  # Increase or decrease this value to adjust the exposure
+    image_tensor = TF.to_tensor(image)
+    exposure_factor = 2.5
     adjusted_image_tensor = image_tensor * exposure_factor
     # Clip the pixel values to the valid range [0, 1]
     adjusted_image_tensor = torch.clamp(adjusted_image_tensor, 0.0, 1.0)
-    # Convert the adjusted image tensor back to a PIL image
     adjusted_image = TF.to_pil_image(adjusted_image_tensor)
 
     return adjusted_image
 
 
 def add_noise(image):
-    # Convert the image to a PyTorch tensor
-    image_tensor = TF.to_tensor(image)
+    """
+    add noise to a given image
+    """
 
-    # Generate random Gaussian noise with the same size as the image tensor
+    image_tensor = TF.to_tensor(image)
+    # Generate random Gaussian noise
     noise_tensor = torch.randn_like(image_tensor)
 
-    # Define the noise intensity
     # noise_intensity = 0.2 #groups 1-3
-    noise_intensity = 0.1  # Adjust this value to control the intensity of the noise
+    noise_intensity = 0.1
 
-    # Add the noise to the image tensor
     noisy_image_tensor = image_tensor + noise_intensity * noise_tensor
 
     # Clip the pixel values to the valid range [0, 1]
@@ -154,40 +157,57 @@ def add_noise(image):
 if __name__ == '__main__':
 
     original_dataset_path = os.path.join("..", "data", "new_train")
+
+    # create datasets experiment group 1
     names1 = ["augments_group_1_1","augments_group_1_2","augments_group_1_3"]
-    #group 1
-    # create_datasets(names1,augmentation_transforms=[add_noise,exposure,transforms.GaussianBlur(kernel_size=5, sigma=(0.1, 5)),
-    #                                                transforms.RandomRotation(degrees=(10, 20))],flip_transforms=[])
-    #
-    # names2 = ["augments_group_2_1", "augments_group_2_2", "augments_group_2_3"]
-    # #group 2
+
+    create_datasets(original_dataset_path=original_dataset_path,
+                    dataset_names=names1,
+                    augmentation_transforms=[add_noise,exposure,transforms.GaussianBlur(kernel_size=5, sigma=(0.1, 5)),
+                                                   transforms.RandomRotation(degrees=(10, 20))],
+                    flip_transforms=[],
+                    together_transforms=[],
+                    together_classes=[],
+                    flip_v=False)
+
+    # create datasets experiment group 2
+    names2 = ["augments_group_2_1", "augments_group_2_2", "augments_group_2_3"]
+
     flip_transform = [
         transforms.RandomHorizontalFlip(1),
         transforms.RandomVerticalFlip(1),
     ]
-    # create_datasets(original_dataset_path=original_dataset_path,
-    #                 dataset_names=names2,
-    #                 augmentation_transforms=[transforms.GaussianBlur(kernel_size=9, sigma=(0.1, 5)),
-    #                                                 transforms.RandomRotation(degrees=15)],
-    #                 flip_transforms=flip_transform,together_transforms=[],together_class=[] )
-
-    # names3 = ["augments_group_3_1", "augments_group_3_2", "augments_group_3_3"]
-    #
-    # aug = transforms.Compose([transforms.GaussianBlur(kernel_size=7, sigma=(0.1, 5)),transforms.RandomRotation(degrees=30)])
-    # together_transform = [transforms.RandomHorizontalFlip(1),add_noise]
-    # create_datasets(names3,augmentation_transforms=[aug],flip_transforms=[],
-    #                 together_transforms=together_transform,together_class=["i","ii","iii","v","x"])
-
-    names4 = ["augments_group_4_1", "augments_group_4_2", "augments_group_4_3"]
-    #group 4 includes flipping iv to vi and vi to iv , and v to v
-    #noise is set to 0.1
-
-    # original_dataset_path = os.path.join("..", "data", "copy_of_new_train")
-    aug = transforms.Compose(
-        [transforms.GaussianBlur(kernel_size=7, sigma=(0.1, 5)), transforms.RandomRotation(degrees=15)])
-
-    create_datasets(original_dataset_path=original_dataset_path,dataset_names=names4,
-                    augmentation_transforms=[aug,transforms.GaussianBlur(kernel_size=9, sigma=(0.1, 5)),add_noise,transforms.RandomRotation(degrees=15)],
+    create_datasets(original_dataset_path=original_dataset_path,
+                    dataset_names=names2,
+                    augmentation_transforms=[transforms.GaussianBlur(kernel_size=9, sigma=(0.1, 5)),
+                                                    transforms.RandomRotation(degrees=15)],
                     flip_transforms=flip_transform,
                     together_transforms=[],
-                    together_class=[],flip_v=True)
+                    together_classes=[],
+                    flip_v=False)
+
+    # create datasets experiment group 3
+    names3 = ["augments_group_3_1", "augments_group_3_2", "augments_group_3_3"]
+
+    combined_augmentation = transforms.Compose([transforms.GaussianBlur(kernel_size=7, sigma=(0.1, 5)), transforms.RandomRotation(degrees=30)])
+    together_transform = [transforms.RandomHorizontalFlip(1),add_noise]
+    create_datasets(original_dataset_path=original_dataset_path,
+                    dataset_names=names3,
+                    augmentation_transforms=[combined_augmentation],
+                    flip_transforms=[],
+                    together_transforms=together_transform,
+                    together_classes=["i","ii","iii","v","x"],
+                    flip_v=False)
+
+    # create datasets experiment group 4 - to recreate should change noise intensity to 0.1!
+    names4 = ["augments_group_4_1", "augments_group_4_2", "augments_group_4_3"]
+
+    combined_augmentation = transforms.Compose(
+        [transforms.GaussianBlur(kernel_size=7, sigma=(0.1, 5)), transforms.RandomRotation(degrees=15)])
+
+    create_datasets(original_dataset_path=original_dataset_path, dataset_names=names4,
+                    augmentation_transforms=[transforms.GaussianBlur(kernel_size=9, sigma=(0.1, 5)),add_noise,transforms.RandomRotation(degrees=15)],
+                    flip_transforms=flip_transform,
+                    together_transforms=[],
+                    together_classes=[],
+                    flip_v=True)
