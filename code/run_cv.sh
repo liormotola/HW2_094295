7eef3958d186b5c290b1788a995969341f7ae3f7
lior motola <lior.motola@campus.technion.ac.il>
2023-06-03 12:57:16 +0300

    run_cv.sh

---
+++ code/run_cv.sh
@@ -0,0 +1,26 @@
+echo "augments_group_1_1"
+python our_run_train_eval.py ../data/augments_group_1_1  augments_group_1_1.pt augments_group_1_1_loss augments_group_1_1_acc
+
+echo "augments_group_1_2"
+python our_run_train_eval.py ../data/augments_group_1_2  augments_group_1_2.pt augments_group_1_2_loss augments_group_1_2_acc
+
+echo "augments_group_1_3"
+python our_run_train_eval.py ../data/augments_group_1_3  augments_group_1_3.pt augments_group_1_3_loss augments_group_1_3_acc
+
+echo "augments_group_2_1"
+python our_run_train_eval.py ../data/augments_group_2_1  augments_group_2_1.pt augments_group_2_1_loss augments_group_2_1_acc
+
+echo "augments_group_2_2"
+python our_run_train_eval.py ../data/augments_group_2_2  augments_group_2_2.pt augments_group_2_2_loss augments_group_2_2_acc
+
+echo "augments_group_2_3"
+python our_run_train_eval.py ../data/augments_group_2_3  augments_group_2_3.pt augments_group_2_3_loss augments_group_2_3_acc
+
+echo "augments_group_3_1"
+python our_run_train_eval.py ../data/augments_group_3_1  augments_group_3_1.pt augments_group_3_1_loss augments_group_3_1_acc
+
+echo "augments_group_3_2"
+python our_run_train_eval.py ../data/augments_group_3_2  augments_group_3_2.pt augments_group_3_2_loss augments_group_3_2_acc
+
+echo "augments_group_3_3"
+python our_run_train_eval.py ../data/augments_group_3_3  augments_group_3_3.pt augments_group_3_3_loss augments_group_3_3_acc
